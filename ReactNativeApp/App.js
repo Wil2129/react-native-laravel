600ff7dc4d48cd4e24b38bcc60424d34ab569c8f
Teddy BAHA <michael_takeshi@yahoo.com>
2019-08-25 03:31:05 +0100

    UI built and networking implemented

--- ReactNativeApp/App.js
+++ ReactNativeApp/App.js
@@ -6,109 +6,102 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, {Component} from 'react';
 import {
+  Button,
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
+  TextInput,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  textInput: {
+    flex: 1,
+    padding: 8,
+    textAlignVertical: 'top',
+    fontSize: 20,
+    margin: 16,
+    borderRadius: 4,
+    borderColor: '#000000',
+    borderWidth: 1,
   },
-  body: {
-    backgroundColor: Colors.white,
+  textArea: {
+    flex: 3,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
+  submitButton: {
+    alignSelf: 'center',
+    flex: 0.75,
     fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    margin: 150,
+    borderRadius: 8,
   },
 });
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    const url = ''; //The URL of the web server where the Laravel API is running
+    this.state = {name: '', email: '', subject: '', message: ''};
+  }
+
+  _onSubmit() {
+    fetch(this.url + '/api/contact_forms', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        subject: this.state.subject,
+        message: this.state.message,
+      }),
+    })
+      .then(response => alert('Item created successfully'))
+      .catch(error => console.error('Error:', error));
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Name"
+            onChangeText={name => this.setState({name})}
+            value={this.state.name}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email"
+            onChangeText={email => this.setState({email})}
+            value={this.state.email}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Subject"
+            onChangeText={subject => this.setState({subject})}
+            value={this.state.subject}
+          />
+          <TextInput
+            style={[styles.textInput, styles.textArea]}
+            placeholder="Message"
+            onChangeText={message => this.setState({message})}
+            value={this.state.message}
+          />
+          <Button
+            style={styles.submitButton}
+            onPress={this._onSubmit.bind(this)}
+            title="Submit"
+            color="#40C4FF"
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
